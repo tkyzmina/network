6af7ba4e78a572ea48067a3ea5b7999719f163c2
tkyzmina <tkyzmina@gmail.com>
2023-07-26 12:30:19 +0300

    (feat): Add basic folders and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Home from './pages/Home'
+import Contacts from './pages/Contacts'
+import Chat from './pages/Chat'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
-  return <h1>Network</h1>
+  return (
+    <>
+      <Home />
+      <Contacts />
+      <Chat />
+      <Register />
+      <Login />
+    </>
+  )
 }
 
 export default App
